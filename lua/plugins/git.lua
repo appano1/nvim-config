6f6e4c8413e206f05fab2580fe80d937c89cc758
appano1 <thankfather@naver.com>
2024-02-08 18:46:18 +0900

    feat: Add gitsigns plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,12 @@
 return {
 	{ "tpope/vim-fugitive" },
+	{
+		"gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+			vim.keymap.set("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame<CR>")
+		end,
+	},
 }
