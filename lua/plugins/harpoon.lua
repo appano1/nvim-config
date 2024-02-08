7f20d78ca36d5b997fa6afb5d5d5a890257d4903
appano1 <thankfather@naver.com>
2024-02-08 19:13:11 +0900

    refactor: Migrate harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,23 +3,27 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
 		vim.keymap.set("n", "<leader>1", function()
-			ui.nav_file(1)
+			harpoon:list():select(1)
 		end)
 		vim.keymap.set("n", "<leader>2", function()
-			ui.nav_file(2)
+			harpoon:list():select(2)
 		end)
 		vim.keymap.set("n", "<leader>3", function()
-			ui.nav_file(3)
+			harpoon:list():select(3)
 		end)
 		vim.keymap.set("n", "<leader>4", function()
-			ui.nav_file(4)
+			harpoon:list():select(4)
 		end)
 	end,
 }
