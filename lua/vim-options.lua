4d8dc4886cb06f894db4742d90f088e64a6597eb
appano1 <thankfather@naver.com>
2024-02-13 16:56:08 +0900

    refactor: Add vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,29 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set relativenumber")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- undotree are going to save current changes
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
 
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
